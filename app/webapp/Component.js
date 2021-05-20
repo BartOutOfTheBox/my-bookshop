29f94419d18513f732ef935c057d2da5bec94078
Bart Verhoeven <bart.verhoeven@amista.be>
2021-05-20 16:51:07 +0200

    Added Fiori elements application

---
+++ app/webapp/Component.js
@@ -0,0 +1,21 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (AppComponent) {
+        "use strict";
+
+        return AppComponent.extend("my.bookshop.Component", {
+            metadata: {
+                manifest: "json"
+            }, 
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+             init: function () {
+                AppComponent.prototype.init.apply(this, arguments);
+             }
+        });
+    }
+);
